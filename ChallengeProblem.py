b4b0f956c8d517aacf9bc04b7bc16ad472d66efe
Ritam Saha <ritamsaha2005@gmail.com>
2019-10-09 19:19:16 -0700

    Finished Version :)

--- ChallengeProblem.py
+++ ChallengeProblem.py
@@ -1,25 +1,16 @@
 divisor = 0
-dividend = 0
-FoundSolution = False
-
-
-def num_after_point(x):
-    s = str(x)
-    if not '.' in s:
-        return 0
-    return len(s) - s.index('.') - 1
-
-
 for i in range(999):
     dividend = 0
     divisor += 0.01
-    for x in range(99):
+    for z in range(99):
         dividend += .1
-        divisor = round(divisor, 2)
+        x = round(100 * divisor)
+        y = round(100 * dividend)
         dividend = round(dividend, 1)
-        if (num_after_point((dividend / divisor))) == 3:
-            if len(list(str(dividend / divisor))) == 5:
-                if int(list(str(dividend / divisor))[2]) == 0:
-         #           k = float("0" + "".join(list(str(dividend/divisor))[1:]))
-  #                  print(k)
-
+        divisor = round(divisor, 2)
+        k = y % x
+        if k != 0:
+            if k * 10 % x == k * 10:
+                if k * 100 % x == k * 100:
+                    if k * 1000 % x == 0:
+                        print(dividend, divisor)
--- reee.py
+++
@@ -1,8 +0,0 @@
-def num_after_point(x):
-    s = str(x)
-    print(list(s))
-    if not '.' in s:
-        return 0
-    return len(s) - s.index('.') - 1
-
-print(num_after_point(1/2))
